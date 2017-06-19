2f5cd3622c33af587d50cbf05c58e03e980261dd
Patrick Scuracchio <patrck@usp.br>
2017-06-19 16:08:14 -0300

    Changed all questions to it's right

--- js/forms.js
+++ js/forms.js
@@ -1,8 +1,8 @@
 
 function previousQuestion(btn) {
-    window.location.href = "file:///Users/eupecric/Documents/USP/FAPESP/questao" + btn.value  + ".html";
+    window.location.href = "https://eupecric.github.io/ppsus/questao" + btn.value  + ".html";
 }
 
 function nextQuestion(btn) {
-    window.location.href = "file:///Users/eupecric/Documents/USP/FAPESP/questao" + btn.value  + ".html";
+    window.location.href = "https://eupecric.github.io/ppsus/questao" + btn.value  + ".html";
 }
